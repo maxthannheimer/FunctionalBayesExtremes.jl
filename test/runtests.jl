dfc1912e46eba8442f2ebf8ab97fe2c7e085d7ed
Max Thannheimer <max.thannheimer@mathematik.uni-stuttgart.de>
2026-02-13 18:20:44 +0100

    new test that breaks things

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ using Test
 @testset "FunctionalBayesExtremes.jl" begin
     # 2x+3y
     @test my_f(2,1) == 7
-    # 2x+3y
     @test my_f(2,3) == 13
+    @test my_f(2,3) == 11
 
  
     # Write your tests here.
